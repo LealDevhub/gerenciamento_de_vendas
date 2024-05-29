8c4c8105a0321180e5c49956d824155f4245aa15
fafadevhub <fabriciopereiraleal2021@gmail.com>
2024-05-29 08:01:18 -0300

    Alterando dias da semana no Canvas

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,6 +1,6 @@
 /* globals Chart:false */
 
-(() => {
+;(() => {
   'use strict'
 
   // Graphs
@@ -10,30 +10,24 @@
     type: 'line',
     data: {
       labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+        'Domingo',
+        'Segunda-Feira',
+        'Terça-Feira',
+        'Quarta-Feira',
+        'Quinta-Feira',
+        'Sexta-Feira',
+        'Sábado'
       ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
+      datasets: [
+        {
+          data: [15339, 22345, 18483, 24003, 23489, 24092, 12034],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }
+      ]
     },
     options: {
       plugins: {
